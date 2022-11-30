99e6a7800672c7968ea61f299f7c5a58ca1dd67b
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-11-30 21:55:57 +0900

    타입지정 정리

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-var Name = "kim";
-Name = "123";
-console.log(Name);
-// 함수에도 타입지정이 가능함
-function multi(x) {
-    return x * 2;
-}
-var john = [123, false];
-var kim = { name: "hi" };
-// class에서도 타입지정이 가능
-var UserList = /** @class */ (function () {
-    function UserList(name) {
-        this.name = name;
-    }
-    return UserList;
-}());
+var 이름 = "kim";
+var 나이 = 20;
+var marry = true;
+var 회원들 = ["kim", "park"];
+var 멤버들 = {
+    member1: "kim",
+    member2: "park",
+};
+// ts는 변수 생성시 타입스크립트가 해당 변수의 타입을 자동으로 부여해준다. 즉 일일히 타입을 지정해줄 필요는 없음
+var 이름표 = "kim";
+var 나이는 = 20;
+var userName = "프론트";
+var userAge = 20;
+var musicInfo = {
+    title: "제목",
+    singer: "가수",
+};
+var project = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+};
--- index.ts
+++ index.ts
@@ -1,30 +1,36 @@
-// type 키워드를 통해 타입을 지정할수도 있음
-type test = string | number;
-
-let Name: test = "kim";
-
-Name = "123";
-console.log(Name);
-
-// 함수에도 타입지정이 가능함
-function multi(x: number): number {
-  return x * 2;
-}
-
-// array에 쓸수있는 tuple(튜플) 타입 첫번째는 무조건 넘버 두번째는 불리언으로
-type Member = [number, boolean];
-let john: Member = [123, false];
-
-// Object에 타입 지정 ( 그런데 옵젝에 타입지정해야할 속성이 너무 많을 경우 싸잡아서도 가능 )
-type User = {
-  [key: string]: string;
+let 이름: string = "kim";
+let 나이: number = 20;
+let marry: boolean = true;
+let 회원들: string[] = ["kim", "park"];
+let 멤버들: {
+  member1: string;
+  member2: string;
+} = {
+  member1: "kim",
+  member2: "park",
 };
-let kim: User = { name: "hi" };
 
-// class에서도 타입지정이 가능
-class UserList {
-  name: string;
-  constructor(name: string) {
-    this.name = name;
-  }
-}
+// ts는 변수 생성시 타입스크립트가 해당 변수의 타입을 자동으로 부여해준다. 즉 일일히 타입을 지정해줄 필요는 없음
+let 이름표 = "kim";
+let 나이는 = 20;
+
+let userName: string = "프론트";
+let userAge: number = 20;
+
+const musicInfo: {
+  title: string;
+  singer: string;
+} = {
+  title: "제목",
+  singer: "가수",
+};
+
+const project: {
+  member: string[];
+  days: number;
+  started: boolean;
+} = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
