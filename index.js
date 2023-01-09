a7c5518f67a164b67a4447bb2f333d72222b37d4
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-01-09 22:35:48 +0900

    class 만들때 타입지정

--- index.js
+++ index.js
@@ -10,3 +10,34 @@ var Person = /** @class */ (function () {
 var person1 = new Person("");
 var person2 = new Person("park");
 person1.func("ss");
+// 문제
+var Car = /** @class */ (function () {
+    function Car(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    Car.prototype.tax = function () {
+        return Math.floor(this.price * 0.1);
+    };
+    return Car;
+}());
+var car = new Car("소나타", 3000);
+var Word = /** @class */ (function () {
+    function Word() {
+        var params = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            params[_i] = arguments[_i];
+        }
+        var numbers = [];
+        var strings = [];
+        params.forEach(function (a) {
+            typeof a === "string" ? strings.push(a) : numbers.push(a);
+        });
+        this.num = numbers;
+        this.str = strings;
+    }
+    return Word;
+}());
+var obj = new Word("kim", 3, 5, "park");
+console.log(obj.num);
+console.log(obj.str);
--- index.ts
+++ index.ts
@@ -12,3 +12,40 @@ let person1 = new Person("");
 let person2 = new Person("park");
 
 person1.func("ss");
+
+// 문제
+
+class Car {
+  name: string;
+  price: number;
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+  tax(): number {
+    return Math.floor(this.price * 0.1);
+  }
+}
+
+const car = new Car("소나타", 3000);
+
+class Word {
+  num: number[];
+  str: string[];
+  constructor(...params) {
+    let numbers: number[] = [];
+    let strings: string[] = [];
+
+    params.forEach((a) => {
+      typeof a === "string" ? strings.push(a) : numbers.push(a);
+    });
+
+    this.num = numbers;
+    this.str = strings;
+  }
+}
+
+const obj = new Word("kim", 3, 5, "park");
+
+console.log(obj.num);
+console.log(obj.str);
