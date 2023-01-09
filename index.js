7d94fd24ba13954aa2d10d766e2647098689fc07
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-01-09 23:03:55 +0900

    interface 키워드에 대해

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-var Person = /** @class */ (function () {
-    function Person(userName) {
-        this.name = userName;
-    }
-    Person.prototype.func = function (text) {
-        console.log("hello ".concat(text));
-    };
-    return Person;
-}());
-var person1 = new Person("");
-var person2 = new Person("park");
-person1.func("ss");
-// 문제
-var Car = /** @class */ (function () {
-    function Car(name, price) {
-        this.name = name;
-        this.price = price;
-    }
-    Car.prototype.tax = function () {
-        return Math.floor(this.price * 0.1);
-    };
-    return Car;
-}());
-var car = new Car("소나타", 3000);
-var Word = /** @class */ (function () {
-    function Word() {
-        var params = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            params[_i] = arguments[_i];
-        }
-        var numbers = [];
-        var strings = [];
-        params.forEach(function (a) {
-            typeof a === "string" ? strings.push(a) : numbers.push(a);
-        });
-        this.num = numbers;
-        this.str = strings;
-    }
-    return Word;
-}());
-var obj = new Word("kim", 3, 5, "park");
-console.log(obj.num);
-console.log(obj.str);
+// interface는 중복선언이 가능, type은 중복선언이 불가함
+var nemo = { color: "red", width: 100 };
+var stu = {
+    name: "park",
+};
+var tea = {
+    name: "kim",
+    age: 20,
+};
+var cat = {
+    name: "beria",
+    age: 50,
+};
+var item = {
+    brand: "samsung",
+    serialNumber: 1360,
+    model: ["TV", "phone"],
+};
+var cart = [
+    {
+        product: "청소기",
+        price: 700,
+    },
+    {
+        product: "삼다수",
+        price: 800,
+    },
+];
+var card = [
+    {
+        product: "청소기",
+        price: 7000,
+        card: false,
+    },
+];
+var pm = {
+    plus: function (a, b) {
+        return a + b;
+    },
+    minus: function (a, b) {
+        return a - b;
+    },
+};
--- index.ts
+++ index.ts
@@ -1,51 +1,98 @@
-class Person {
-  name: string;
-  constructor(userName: string) {
-    this.name = userName;
-  }
-  func(text: string) {
-    console.log(`hello ${text}`);
-  }
+// interface는 중복선언이 가능, type은 중복선언이 불가함
+
+// interface 의 장점 : extends로 복사가능
+
+interface Square {
+  color: string;
+  width: number;
 }
 
-let person1 = new Person("");
-let person2 = new Person("park");
+let nemo: Square = { color: "red", width: 100 };
 
-person1.func("ss");
+interface Student {
+  name: string;
+}
+
+interface Teacher extends Student {
+  age: number;
+}
+
+let stu: Student = {
+  name: "park",
+};
+
+let tea: Teacher = {
+  name: "kim",
+  age: 20,
+};
+
+// type 키워드로도 가능함 인터섹션 교차 타입
+// 왼쪽, 오른쪽 둘다 만족해주는 타입을 만들어 달라
+type Animal = {
+  name: string;
+};
+
+type Cat = {
+  age: number;
+} & Animal;
+
+let cat: Cat = {
+  name: "beria",
+  age: 50,
+};
 
 // 문제
 
-class Car {
-  name: string;
+interface Item {
+  brand: string;
+  serialNumber: number;
+  model: string[];
+}
+
+const item: Item = {
+  brand: "samsung",
+  serialNumber: 1360,
+  model: ["TV", "phone"],
+};
+
+interface Cart {
+  product: string;
   price: number;
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
-  tax(): number {
-    return Math.floor(this.price * 0.1);
-  }
 }
 
-const car = new Car("소나타", 3000);
-
-class Word {
-  num: number[];
-  str: string[];
-  constructor(...params) {
-    let numbers: number[] = [];
-    let strings: string[] = [];
-
-    params.forEach((a) => {
-      typeof a === "string" ? strings.push(a) : numbers.push(a);
-    });
-
-    this.num = numbers;
-    this.str = strings;
-  }
+interface Card extends Cart {
+  card: boolean;
 }
 
-const obj = new Word("kim", 3, 5, "park");
+const cart: Cart[] = [
+  {
+    product: "청소기",
+    price: 700,
+  },
+  {
+    product: "삼다수",
+    price: 800,
+  },
+];
 
-console.log(obj.num);
-console.log(obj.str);
+const card: Card[] = [
+  {
+    product: "청소기",
+    price: 7000,
+    card: false,
+  },
+];
+
+interface PMresult {
+  plus: (a: number, b: number) => number;
+  minus: (a: number, b: number) => number;
+}
+
+const pm: PMresult = {
+  plus(a, b) {
+    return a + b;
+  },
+  minus(a, b) {
+    return a - b;
+  },
+};
