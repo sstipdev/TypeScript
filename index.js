f9ec5799fc85f17b11b48b600f90fa1569521bf3
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-01-09 21:53:45 +0900

    class 만들때 타입지정

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-// TypeScript로 html 변경과 조작할때
-/**
- * 1.narrowing 문법
- * 2.instanceof 문법
- * 3.as 키워드
- * 4.오브젝트 옵셔널 체이닝 ?.
- * 5.strict 모드 끄기
- */
-var title = document.querySelector("#title");
-if (title !== null) {
-    title.innerHTML = "테스트";
-}
-var link = document.querySelector(".link");
-if (link instanceof HTMLAnchorElement) {
-    link.href = "https://google.com";
-}
-var button = document.querySelector("#button");
-button === null || button === void 0 ? void 0 : button.addEventListener("click", function () { });
-/** 문제 */
-var img = document.querySelector("#image");
-if (img instanceof HTMLImageElement) {
-    img.src = "new.jpg";
-}
-var naver = document.querySelectorAll(".naver");
-naver.forEach(function (a) {
-    if (a instanceof HTMLAnchorElement) {
-        a.href = "https://kakao.com";
+var Person = /** @class */ (function () {
+    function Person(userName) {
+        this.name = userName;
     }
-});
+    Person.prototype.func = function (text) {
+        console.log("hello ".concat(text));
+    };
+    return Person;
+}());
+var person1 = new Person("");
+var person2 = new Person("park");
+person1.func("ss");
--- index.ts
+++ index.ts
@@ -1,35 +1,14 @@
-// TypeScript로 html 변경과 조작할때
-/**
- * 1.narrowing 문법
- * 2.instanceof 문법
- * 3.as 키워드
- * 4.오브젝트 옵셔널 체이닝 ?.
- * 5.strict 모드 끄기
- */
-
-let title = document.querySelector("#title");
-if (title !== null) {
-  title.innerHTML = "테스트";
-}
-
-let link = document.querySelector(".link");
-if (link instanceof HTMLAnchorElement) {
-  link.href = "https://google.com";
-}
-
-let button = document.querySelector("#button");
-button?.addEventListener("click", function () {});
-
-/** 문제 */
-
-const img = document.querySelector("#image");
-if (img instanceof HTMLImageElement) {
-  img.src = "new.jpg";
-}
-
-const naver = document.querySelectorAll(".naver");
-naver.forEach((a) => {
-  if (a instanceof HTMLAnchorElement) {
-    a.href = "https://kakao.com";
+class Person {
+  name: string;
+  constructor(userName: string) {
+    this.name = userName;
   }
-});
+  func(text: string) {
+    console.log(`hello ${text}`);
+  }
+}
+
+let person1 = new Person("");
+let person2 = new Person("park");
+
+person1.func("ss");
