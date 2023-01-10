7f86e9cee9902c236864cbe742d6a9d78ad350ed
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-01-10 22:32:19 +0900

    Narrowing 할수 있는 방법 추가로 알아보기

--- index.js
+++ index.js
@@ -1,43 +1,19 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
+// in 키워드로 object narrowing 가능 속성명 in 오브젝트자료
+function fuc(animal) {
+    if ("swim" in animal) {
+        animal.swim;
     }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-// Rest 파라미터를 쓰면 파라미터를 여러개 들어올수가 있음
-function 함수() {
-    var a = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        a[_i] = arguments[_i];
+}
+// 오브젝트 instanceof 부모class
+var date = new Date();
+if (date instanceof Date) {
+    console.log(1);
+}
+function 함수(x) {
+    if (x.wheel === "4개") {
+        console.log("the car is " + x.color);
     }
-    console.log(a);
+    else {
+        console.log("the bike is " + x.color);
+    }
 }
-함수(1, 2, 3, 54, 6, 6, 1, 23, 4, 5, 1);
-var arr = [1, 2, 3];
-var arr2 = [4, 5];
-var arr3 = __spreadArray(__spreadArray([], arr, true), arr2, true);
-console.log(arr3);
-// destructuring 문법
-var text = ["안녕", 100];
-var _a = ["안녕", 100], text1 = _a[0], text2 = _a[1];
-var _b = { student: true, age: 20 }, student = _b.student, age = _b.age;
-// 문제
-var person = { student: true, age: 20 };
-function perFunc(_a) {
-    var student = _a.student, age = _a.age;
-    console.log(student, age);
-}
-perFunc({ student: true, age: 23 });
-function userInfo(_a) {
-    var user = _a.user, comment = _a.comment, admin = _a.admin;
-    console.log(user, comment, admin);
-}
-userInfo({ user: "kim", comment: [1, 5, 4, 2], admin: true });
-function getWine(_a) {
-    var a = _a[0], b = _a[1], c = _a[2];
-    console.log(a, b, c);
-}
-getWine([40, "winw", false]);
--- index.ts
+++ index.ts
@@ -1,40 +1,35 @@
-// Rest 파라미터를 쓰면 파라미터를 여러개 들어올수가 있음
-function 함수(...a: number[]) {
-  console.log(a);
+// in 키워드로 object narrowing 가능 속성명 in 오브젝트자료
+
+type Fish = { swim: string };
+type Bird = { fly: string };
+
+function fuc(animal: Fish | Bird) {
+  if ("swim" in animal) {
+    animal.swim;
+  }
 }
 
-함수(1, 2, 3, 54, 6, 6, 1, 23, 4, 5, 1);
+// 오브젝트 instanceof 부모class
 
-let arr = [1, 2, 3];
-let arr2 = [4, 5];
-let arr3 = [...arr, ...arr2];
-console.log(arr3);
-
-// destructuring 문법
-const text = ["안녕", 100];
-const [text1, text2] = ["안녕", 100];
-
-const { student, age } = { student: true, age: 20 };
-
-// 문제
-let person = { student: true, age: 20 };
-
-function perFunc({ student, age }: { student: boolean; age: number }) {
-  console.log(student, age);
+let date = new Date();
+if (date instanceof Date) {
+  console.log(1);
 }
 
-perFunc({ student: true, age: 23 });
+type Car = {
+  wheel: "4개";
+  color: string;
+};
 
-function userInfo({ user, comment, admin }: { user: string; comment: number[]; admin: boolean }) {
-  console.log(user, comment, admin);
+type Bike = {
+  wheel: "2개";
+  color: string;
+};
+
+function 함수(x: Car | Bike) {
+  if (x.wheel === "4개") {
+    console.log("the car is " + x.color);
+  } else {
+    console.log("the bike is " + x.color);
+  }
 }
-
-userInfo({ user: "kim", comment: [1, 5, 4, 2], admin: true });
-
-type Wine = (number | string | boolean)[];
-
-function getWine([a, b, c]: Wine) {
-  console.log(a, b, c);
-}
-
-getWine([40, "winw", false]);
