bafdd0bd3635b60a79934a7da7dc27edf8fd0820
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-01-11 21:57:21 +0900

    class protected, static 키워드에 대해 (문제풀이)

--- index.js
+++ index.js
@@ -1,30 +1,96 @@
 /**
- * - class에서 쓰는 public 키워드
- * 1.public 붙으면 모든 자식들이 이용이 가능함
- *
- * - class에서 쓰는 private 키워드
- * 1.private 붙으면 class안에서만 수정, 이용 가능함 외부에선 사용 및 수정 불가
- * 정말 변경해야될 상황이 온다면, 메서드 함수를 하나 새로 생성하여서 변경이 가능함
+ * - protected 키워드는 private처럼 class {} 안에서만 사용이 가능하다
+ * 확장성을 제공한다.
+ *  private 키워드는 extends된 클래스에선 쓸순 없지만, protected는 쓸수 있음.
+ *  protected, private는 공통적으로 자식들은 사용이 불가능하다.
  */
-var User = /** @class */ (function () {
-    function User(a) {
-        this.familyName = "kim";
-        this.name = this.familyName + a;
-    }
-    User.prototype.changeName = function (newName) {
-        this.familyName = newName;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
     };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var User = /** @class */ (function () {
+    function User() {
+        this.x = 10;
+    }
     return User;
 }());
-var user = new User("in");
-user.changeName("choi");
-console.log(user);
-var Person = /** @class */ (function () {
-    function Person(name) {
-        this.name = name;
-        this.name = name;
+var NewUser = /** @class */ (function (_super) {
+    __extends(NewUser, _super);
+    function NewUser() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-    return Person;
+    NewUser.prototype.doThis = function () {
+        this.x = 215;
+    };
+    return NewUser;
+}(User));
+var user1 = new User();
+var user2 = new NewUser();
+user2.doThis();
+console.log(user1, user2);
+// static 키워드는 부모 class에만 직접부여되고, 자식에겐 물려주지 않는다.
+var GetUser = /** @class */ (function () {
+    function GetUser() {
+        this.y = 20;
+    }
+    GetUser.x = 10;
+    return GetUser;
 }());
-var child = new Person("1541");
-console.log(child);
+var child = new GetUser();
+var UserInfo = /** @class */ (function () {
+    function UserInfo() {
+        this.intro = "".concat(UserInfo.skill, "\uC804\uBB38\uAC00");
+    }
+    UserInfo.skill = "js";
+    return UserInfo;
+}());
+var userInfo1 = new UserInfo();
+console.log(userInfo1);
+UserInfo.skill = "ts";
+var userInfo2 = new UserInfo();
+console.log(userInfo2);
+// 문제
+var AddUser = /** @class */ (function () {
+    function AddUser() {
+    }
+    AddUser.addOne = function (num) {
+        AddUser.x += num;
+    };
+    AddUser.printX = function () {
+        console.log(AddUser.x);
+    };
+    AddUser.x = 10;
+    AddUser.y = 20;
+    return AddUser;
+}());
+console.log(AddUser.addOne(5));
+AddUser.printX();
+var Square = /** @class */ (function () {
+    function Square(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    Square.prototype.draw = function () {
+        var place = Math.random();
+        var square = "<div style=\"position:relative; \n    top:".concat(place * 400, "px;\n    left:").concat(place * 400, "px;\n    width:").concat(this.width, "px;\n    height:").concat(this.height, "px;\n    background:").concat(this.color, "\"></div>");
+        document.body.insertAdjacentHTML("beforeend", square);
+    };
+    return Square;
+}());
+var square = new Square(50, 50, "blue");
+square.draw();
+square.draw();
+square.draw();
+square.draw();
--- index.ts
+++ index.ts
@@ -1,32 +1,79 @@
 /**
- * - class에서 쓰는 public 키워드
- * 1.public 붙으면 모든 자식들이 이용이 가능함
- *
- * - class에서 쓰는 private 키워드
- * 1.private 붙으면 class안에서만 수정, 이용 가능함 외부에선 사용 및 수정 불가
- * 정말 변경해야될 상황이 온다면, 메서드 함수를 하나 새로 생성하여서 변경이 가능함
+ * - protected 키워드는 private처럼 class {} 안에서만 사용이 가능하다
+ * 확장성을 제공한다.
+ *  private 키워드는 extends된 클래스에선 쓸순 없지만, protected는 쓸수 있음.
+ *  protected, private는 공통적으로 자식들은 사용이 불가능하다.
  */
 
 class User {
-  name: string;
-  private familyName: string = "kim";
-  constructor(a: string) {
-    this.name = this.familyName + a;
-  }
-  changeName(newName: string) {
-    this.familyName = newName;
+  protected x = 10;
+}
+
+class NewUser extends User {
+  doThis() {
+    this.x = 215;
   }
 }
 
-const user = new User("in");
-user.changeName("choi");
-console.log(user);
+const user1 = new User();
+const user2 = new NewUser();
+user2.doThis();
+console.log(user1, user2);
 
-class Person {
-  constructor(public name: string) {
-    this.name = name;
+// static 키워드는 부모 class에만 직접부여되고, 자식에겐 물려주지 않는다.
+
+class GetUser {
+  private static x = 10;
+  y = 20;
+}
+const child = new GetUser();
+
+class UserInfo {
+  static skill = "js";
+  intro = `${UserInfo.skill}전문가`;
+}
+
+const userInfo1 = new UserInfo();
+console.log(userInfo1);
+
+UserInfo.skill = "ts";
+const userInfo2 = new UserInfo();
+console.log(userInfo2);
+
+// 문제
+
+class AddUser {
+  private static x = 10;
+  public static y = 20;
+
+  static addOne(num: number) {
+    AddUser.x += num;
+  }
+
+  static printX() {
+    console.log(AddUser.x);
   }
 }
 
-const child = new Person("1541");
-console.log(child);
+console.log(AddUser.addOne(5));
+AddUser.printX();
+
+class Square {
+  constructor(public width: number, public height: number, public color: string) {}
+  draw() {
+    let place = Math.random();
+    let square = `<div style="position:relative; 
+    top:${place * 400}px;
+    left:${place * 400}px;
+    width:${this.width}px;
+    height:${this.height}px;
+    background:${this.color}"></div>`;
+    document.body.insertAdjacentHTML("beforeend", square);
+  }
+}
+
+const square = new Square(50, 50, "blue");
+square.draw();
+square.draw();
+square.draw();
+square.draw();
